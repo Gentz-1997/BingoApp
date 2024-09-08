c3a23f73d73f9dd12851106224c50047ae3f5b02
Matsumi Genki <genki.kuromu@gmail.com>
2024-09-08 15:15:45 +0900

    complited

--- js/main.js
+++ js/main.js
@@ -1,23 +1,57 @@
-'use-strict';
+'use strict';
 
 {
-  // const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  const source = [];
+  // 数字を生成
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
 
-  for (let i = 0; i < 15; i++) {
-    source[i] = i + 1;
+  // spliceでランダムに数字を取り出す
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+
+    return column;
   }
 
-  const b = [];
-  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  // 生成された数字を配列に格納
+  function createColumns() {
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
   }
 
-  console.log(b);
+  // 行と列を逆転させる
+  // function createBingo(columns) {
+  //   const bingo = [];
+  //   for (row = 0; row < 5; row++) {
+  //     bingo[row] = [];
+  //     for (col = 0; col < 5; col++) {
+  //       bingo[row][col] = columns[col][row];
+  //     }
+  //   }
+  //   return bingo;
+  // }
+  
+  // ページに反映
+  function renderBingo(columns) {
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
 }
